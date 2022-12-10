954b3308fcdbb80384a10cf3aff7c67a6d5089ba
siddharth2514 <113873485+siddharth2514@users.noreply.github.com>
2022-12-10 23:20:32 +0530

    Update tiktactoe.py

--- tiktactoe.py
+++ tiktactoe.py
@@ -1,5 +1,6 @@
 import os
-
+s=input("Enter your name User 1: ")
+q=input("Enter your name User 2: ")
 def printBoard(gameValues):
     # Printing Board By using gameValues 's List
     print(f" {gameValues[0]} | {gameValues[1]} | {gameValues[2]} ")
@@ -16,24 +17,27 @@ def checkWin(gameValues):
         # if gameValues matches with the pattern and has X then X won the Match
         if(gameValues[win[0]] == gameValues[win[1]] == gameValues[win[2]] == 'X'):
             printBoard(gameValues)
-            print("X Won the match")
+            print(s,"Won the match")
+            print("Better luck next time",q)
             return 1
 
         # if gameValues matches with the pattern and has X then O won the Match
         if(gameValues[win[0]] == gameValues[win[1]] == gameValues[win[2]] == 'O'):
             printBoard(gameValues)
-            print("O Won the match")
+            print(q,"Won the match")
+            print("Better luck next time",s)
             return 0
 
         # if all places are filled and no one is the winner
         if all(isinstance(item, str) for item in gameValues):
             printBoard(gameValues)
             return -2
-    # return -1 if no one wons
+    # return -1 if no one wins
     return -1
 
 if __name__ == '__main__':
-    print("Welcome to the Game")
+    print("Welcome to the Game",s,"and",q)
+    print("Good luck!")
     gameValues=[0, 1, 2, 3, 4, 5, 6, 7, 8]
     chance = 1
 
@@ -41,35 +45,35 @@ if __name__ == '__main__':
         try:
             if chance == 1:
                 printBoard(gameValues)
-                print("\nX's Chance")
+                print(s,"It's your Chance")
                 value = int(input("\nPlease enter a value: "))
 
                 # check if already filled with O
                 if gameValues[value]!= 'O':
                     gameValues[value] = 'X'
                 else:
-                    os.system('CLS')
+                   
                     print("\nPlease Enter Different Location for X")
                     continue
-                os.system('CLS')
+                
 
             if chance == 0:
                 printBoard(gameValues)
-                print("\nZ's Chance")
+                print(q,"It's your Chance")
                 value = int(input("\nPlease enter a value: "))
 
                 # check if already filled with X
                 if gameValues[value]!= 'X':
                     gameValues[value] = 'O'
                 else:
-                    os.system('CLS')
+                   
                     print("\nPlease Enter Different Location for O")
                     continue
-                os.system('CLS')
+               
 
         except IndexError:
             # exception if Value is not between 0 to 8 
-            os.system('CLS')
+            
             print("\nOops!! Please Enter value from 0 - 8\n")
             continue
 
@@ -78,9 +82,9 @@ if __name__ == '__main__':
         cwin = checkWin(gameValues)
         # Game Draw
         if(cwin == -2):
-            print("Game Draw")
+            print("Phew!It's a draw.\nWhat a match. Looks like you both are geniuse!!.")
             break
         # Match Over
         if(cwin != -1):
-            print("Match over")
+            print("Congratulations on your win!!")
             break
